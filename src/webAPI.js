2e39a8708dab5338e5de81d6982ad1aac7d407b5
krebikshaw <krebikshaw@gmail.com>
2021-01-12 18:29:37 +0800

    feature: app

--- src/webAPI.js
+++ src/webAPI.js
@@ -1,10 +1,10 @@
-const BASE_URL = 'https://api.github.com/user/repos';
+const BASE_URL = 'https://api.github.com/users/krebikshaw/repos';
 
 const getReposAPI = () => {
   return fetch(`${BASE_URL}`, {
     method: 'GET',
     headers: {
-      Authorization: 'token b158363e582e00aa4a76f8b6827ba0d3a9db0367',
+      // Authorization: 'token b158363e582e00aa4a76f8b6827ba0d3a9db0367',
       'content-type': 'application/json',
       Accept: 'application/vnd.github.v3+json',
     },
